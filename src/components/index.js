dc84d7e44157eccbf740eadd7c81aa30ae35400c
8areika <8@afrika.dev>
2023-04-14 23:41:25 +0300

    feat: responsive navbar done and added assets

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import SubHeading from './SubHeading/SubHeading';
 import Newsletter from './Footer/Newsletter';
-import FooterOverlay from './Footer/FooterOverlay';
+import FooterOverlay from './Footer/FooterOverlays';
 import MenuItem from './Menuitem/MenuItem';
 import Navbar from './Navbar/Navbar';
 
